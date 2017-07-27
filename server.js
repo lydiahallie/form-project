a3a034fdb4357bc25366ef897e05224ef690352c
lydiahallie <lydiajuliettehallie@gmail.com>
2017-07-27 16:25:26 -0400

    Added validators

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.engine('mustache', mustacheExpress())
 app.set('views', './views')
 app.set('view engine', 'mustache')
 
+app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(expressValidator())
 
@@ -19,15 +20,44 @@ app.get("/", (req, res) => {
 })
 
 app.post("/", (req, res) => {
-  console.log("body", req.body);
-  res.render('result', {
-    birthyear: req.body.birthyear,
-    email: req.body.email,
-    fullName: req.body.fullName,
-    password: req.body.password,
-    position: req.body.position,
-  });
+  req
+    .checkBody("fullName", "You must give me your email")
+    .notEmpty();
+
+  req
+    .checkBody("email", "We need your full name")
+    .notEmpty();
+
+  req
+    .checkBody("birthyear", "We need your full name")
+    .notEmpty();
+
+  req
+    .checkBody("position", "We need a valid position")
+    .notEmpty();
+
+  req
+    .checkBody("password", "We need a password")
+    .notEmpty();
+
+  const errors = req.validationErrors()
+  console.log(errors)
+  if (errors){
+    const data = {
+      errors: errors
+    }
+    res.render('home', data)
+  } else{
+      res.render('result', {
+        fullName: req.body.fullName,
+        email: req.body.email,
+        birthyear: req.body.birthyear,
+        position: req.body.position,
+        password: req.body.password
+      })
+  }
 });
+
 
 app.listen(3000, () => {
   console.log('Listening on port 3000')
